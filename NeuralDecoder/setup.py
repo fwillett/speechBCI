8ddd17cc994838ec1ab1e65babbb72862368b00d
cfan <wjruoxue@gmail.com>
2023-07-18 15:02:37 -0700

    Update setup.py

--- NeuralDecoder/setup.py
+++ NeuralDecoder/setup.py
@@ -6,8 +6,9 @@ setup(
     packages=find_packages(include=['neuralDecoder']),
     install_requires=[
         'tensorflow-gpu==2.7.0',
-        'hydra-core==1.1.0',
+        'hydra-core==1.3.2',
         'hydra-submitit-launcher==1.1.5',
+        'hydra-optuna-sweeper==1.2.0',
         'transformers==4.28.1',
         'redis',
         'seaborn',
@@ -17,9 +18,13 @@ setup(
         'tqdm',
         'g2p_en==2.1.0',
         'seaborn==0.12.2',
-        'scipy==1.25.0',
+        'numpy==1.25.0',
+        'scipy==1.11.1',
         'torch==1.13.1',
         'accelerate==0.20.3',
-        'bitsandbytes==0.39.1'
+        'bitsandbytes==0.39.1',
+        'edit_distance==1.0.6',
+        'wandb==0.15.5',
+        'hiplot==1.16.0'
     ]
 )
